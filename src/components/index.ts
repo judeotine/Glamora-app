c49c80278b12ceede30e175f49188cf6615a43df
Judeotine <75664161+judeotine@users.noreply.github.com>
2025-10-15 01:25:31 +0300

    feat: export atomic design system components with organized barrel exports (src/components/index.ts)

---
+++ src/components/index.ts
@@ -0,0 +1,19 @@
+export { Button } from './atoms/Button';
+export { Input } from './atoms/Input';
+export { Icon } from './atoms/Icon';
+export { Badge } from './atoms/Badge';
+export { Avatar } from './atoms/Avatar';
+export { Spinner } from './atoms/Spinner';
+export { Text } from './atoms/Text';
+
+export { Card } from './molecules/Card';
+export { Chip } from './molecules/Chip';
+export { SearchBar } from './molecules/SearchBar';
+export { StarRating } from './molecules/StarRating';
+export { PriceTag } from './molecules/PriceTag';
+export { Toggle } from './molecules/Toggle';
+export { ProgressBar } from './molecules/ProgressBar';
+
+export { ImageCarousel } from './organisms/ImageCarousel';
+export { ProductCard } from './organisms/ProductCard';
+
