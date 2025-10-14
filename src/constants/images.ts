f49dbbd84345010ee01a4a8f9aae0a7760584228
Judeotine <75664161+judeotine@users.noreply.github.com>
2025-10-15 01:42:08 +0300

    feat: define image asset references and placeholder URLs (src/constants/images.ts)

---
+++ src/constants/images.ts
@@ -0,0 +1,15 @@
+export const images = {
+  logo: require('@assets/images/logo.png'),
+  logoWhite: require('@assets/images/logo-white.png'),
+  onboarding1: 'https://images.unsplash.com/photo-1515562141207-7a88fb7ce338?w=800',
+  onboarding2: 'https://images.unsplash.com/photo-1599643478518-a784e5dc4c8f?w=800',
+  onboarding3: 'https://images.unsplash.com/photo-1601121141461-9d6647bca1ed?w=800',
+  emptyCart: require('@assets/images/empty-cart.png'),
+  emptyWishlist: require('@assets/images/empty-wishlist.png'),
+  emptyGallery: require('@assets/images/empty-gallery.png'),
+  errorIllustration: require('@assets/images/error.png'),
+  maintenanceIllustration: require('@assets/images/maintenance.png'),
+};
+
+export default images;
+
