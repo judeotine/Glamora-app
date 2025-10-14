3c6cd30bce84bc5da8d35fc4ee2a64cad1914460
Judeotine <75664161+judeotine@users.noreply.github.com>
2025-10-15 00:49:16 +0300

    feat: export custom hooks for authentication, design, cart, and wishlist management (src/hooks/index.ts)

---
+++ src/hooks/index.ts
@@ -0,0 +1,8 @@
+export { useAccessibility } from './useAccessibility';
+export { useNotifications } from './useNotifications';
+export { useDeepLinking } from './useDeepLinking';
+export { useAuth } from './useAuth';
+export { useDesigns } from './useDesigns';
+export { useCart } from './useCart';
+export { useWishlist } from './useWishlist';
+
