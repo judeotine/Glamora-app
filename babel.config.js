e51133da9701b28f2d41ebc6e48d899f23c6a21a
Judeotine <75664161+judeotine@users.noreply.github.com>
2025-10-15 01:00:59 +0300

    chore: configure Babel with Expo preset and module resolver for path aliases (babel.config.js)

---
+++ babel.config.js
@@ -0,0 +1,31 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      'react-native-reanimated/plugin',
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: {
+            '@': './src',
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@navigation': './src/navigation',
+            '@store': './src/store',
+            '@services': './src/services',
+            '@hooks': './src/hooks',
+            '@utils': './src/utils',
+            '@theme': './src/theme',
+            '@types': './src/types',
+            '@constants': './src/constants',
+            '@assets': './assets',
+          },
+        },
+      ],
+    ],
+  };
+};
+
